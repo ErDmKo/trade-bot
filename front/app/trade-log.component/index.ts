369688ecacdbcf23f62fc8d707ca2eb714950233
ErDmKo <erdmko@gmail.com>
2017-03-21 23:21:29 +0300

    Animated log items

--- front/app/trade-log.component/index.ts
+++ front/app/trade-log.component/index.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { 
+    Component,
+    transition,
+    state,
+    style,
+    trigger,
+    animate
+} from '@angular/core';
 import { TradeLogService } from './trade-log.service'
 
 interface iLog {
@@ -14,6 +21,27 @@ interface iLog {
   templateUrl: './template.html',
   providers: [
     TradeLogService
+  ],
+  animations: [
+      trigger('flyInOut', [
+          state('in', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+          })),
+          transition('void => *', [
+            style({
+              opacity: 0,
+              transform: 'translateX(-100%)'
+            }),
+            animate('0.2s ease-in')
+          ]),
+          transition('* => void', [
+            animate('0.2s 10 ease-out', style({
+              opacity: 0,
+              transform: 'translateX(100%)'
+            }))
+          ])
+      ])
   ]
 })
 export class TradeLogComponent {
@@ -30,8 +58,8 @@ export class TradeLogComponent {
             .getWsData()
             .subscribe(
                 data => {
-                    this.data.push(data);
-                    this.data = this.data.slice(-30);
+                    this.data.splice(0, 0, data);
+                    this.data = this.data.slice(0, 30);
                 },
                 error => this.errorMessage = <any>error
             );
